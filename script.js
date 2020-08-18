365c170641a7eb0fa198489809fa7057453fc725
Darshini <55615942+Darshinikumar@users.noreply.github.com>
2020-08-18 23:44:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function lightBulb() {
+  var image = document.getElementById('myBulb');
+  if (image.src.match("on")) {
+    image.src = "lightbulb.svg";
+  } else {
+    image.src = "lightbulb on.svg";
+  }
+}
+
+function lightBulb2() {
+  var image = document.getElementById('myBulb2');
+  if (image.src.match("on")) {
+    image.src = "lightbulb.svg";
+  } else {
+    image.src = "lightbulb on.svg";
+  }
+}
+
+
+function lightBulb3() {
+  var image = document.getElementById('myBulb3');
+  if (image.src.match("on")) {
+    image.src = "lightbulb.svg";
+  } else {
+    image.src = "lightbulb on.svg";
+  }
+}
+
+
+function lightBulb4() {
+  var image = document.getElementById('myBulb4');
+  if (image.src.match("on")) {
+    image.src ="lightbulb.svg";
+  } else {
+    image.src = "lightbulb on.svg";
+  }
+}
+
+
+
